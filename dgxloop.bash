89b67390e7a4382a6c705a2152cf79867c49eaa6
aritify <henrythong93@gmail.com>
2025-05-19 17:36:57 -0700

    Update dgxloop.bash

--- dgxloop.bash
+++ dgxloop.bash
@@ -9,7 +9,7 @@
 #SBATCH  --mail-user=mjb@oregonstate.edu
 for t in 2048 8192 131072 2097152
 do
-        for b in 8 16 32 64 128
+        for b in 8 16 32 64 128 256
         do
                 /usr/local/apps/cuda/11.7/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
                 ./proj05
