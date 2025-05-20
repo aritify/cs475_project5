6e7265db91f8352b10a2ed2a5a55395d11e89587
aritify <henrythong93@gmail.com>
2025-05-19 17:35:55 -0700

    Create dgxloop.bash

---
+++ dgxloop.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH  -J  MonteCarlo
+#SBATCH  -A  cs475-575
+#SBATCH  -p  classgputest
+#SBATCH  --gres=gpu:1
+#SBATCH  -o  montecarlo.out
+#SBATCH  -e  montecarlo.err
+#SBATCH  --mail-type=BEGIN,END,FAIL
+#SBATCH  --mail-user=mjb@oregonstate.edu
+for t in 2048 8192 131072 2097152
+do
+        for b in 8 16 32 64 128
+        do
+                /usr/local/apps/cuda/11.7/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
+                ./proj05
+        done
+done
