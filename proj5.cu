ec19d46bcaea489c3f4d67c1fef8a8dafdf46c1a
aritify <henrythong93@gmail.com>
2025-05-24 19:26:11 -0700

    Update proj5.cu

--- proj5.cu
+++ proj5.cu
@@ -216,6 +216,7 @@ main( int argc, char* argv[ ] )
 
 	// compute the probability:
 	float probability = 100.f * (float)numHits / (float)NUMTRIALS;
+	fprintf( stderr, "%d\n", numHits );
 
 //#define CSV
 
